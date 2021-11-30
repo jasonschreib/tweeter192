f884fb21fdf472dcfda4ed0836957291e26bc5b7
jasonschreib <jwschreiber1@gmail.com>
2021-11-29 23:53:39 -0500

    Created model for tweet and created superuser, working on model for hashtag

--- tweeter/tweeterApp/admin.py
+++ tweeter/tweeterApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from tweeterApp.models import Tweet
 
 # Register your models here.
+admin.site.register(Tweet)
--- tweeter/tweeterApp/models.py
+++ tweeter/tweeterApp/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-# class Tweet(models.Model):
+#model for tweets
+class Tweet(models.Model):
+  body = models.TextField()
+  timestamp = models.DateTimeField(auto_now = True)
+  author = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+  likes = models.IntegerField(default = 0)
+
+
+#model for hashtags
+#class Hashtags(models.Model);
