2d3bffeadb9378973390952a19ef7ab5d70b01dc
jasonschreib <jwschreiber1@gmail.com>
2021-11-30 00:17:39 -0500

    adding functionality for homepage view to query the model and retrieve all tweets

--- tweeter/tweeterApp/views.py
+++ tweeter/tweeterApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+#import the tweet class, hashtag class
+from tweeterApp.models import tweet, hashtag
 
 # Create your views here.
 
@@ -12,9 +14,15 @@ def login(request):
   return render(request, 'login.html', {})
 
 #home page view - all tweets and hashtags
-
+def home(request):
+  #retrieve all instances of the tweet class
+  tweets = tweet.objects.all()
+  return render(request, 'home.html', {"tweets": tweets}) #want to pass in all the tweets, do we need to pass in the user?
 
 #profile page view - specific users tweets
-
+def profile(request):
+  return render(request, 'profile.html', {}) #want to pass in the user - and get the user's tweets
 
 #hashtag page view - all tweets that correspond to a hashtag
+def hashtag(request):
+  return render(request, 'hashtag.html', {}) #want to pass in the hashtag - and get all the tweets that use the hashtag
